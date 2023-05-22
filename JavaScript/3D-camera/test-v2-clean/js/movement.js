0230a120d75953bd2149d8748cc78146997bac0f
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-05-22 15:26:41 +0300

    3D test 2 clean

---
+++ JavaScript/3D-camera/test-v2-clean/js/movement.js
@@ -0,0 +1,61 @@
+const userKeys = new Set();
+
+window.addEventListener("keydown", ({ code, repeat }) => {
+	if (repeat) return;
+	userKeys.add(code);
+
+	if (code === "ArrowRight") {
+		camera.mouseX += 90;
+		mouseMovement(null, true);
+	} else if (code === "ArrowLeft") {
+		camera.mouseX -= 90;
+		mouseMovement(null, true);
+	}
+});
+
+window.addEventListener("keyup", ({ code }) => userKeys.delete(code));
+window.onblur = () => userKeys.clear();
+
+function movePlayer(curentTime, previousTime = 0) {
+	const deltaTime = curentTime - previousTime;
+	const moveSpeed = (size * 5) * deltaTime / 1000;
+
+	if (userKeys.has("KeyW")) {
+		camera.z += moveSpeed * Math.cos((camera.mouseX + 180) * Math.PI / 180)
+		camera.x += moveSpeed * Math.sin(camera.mouseX * Math.PI / 180)
+	}
+	if (userKeys.has("KeyA")) {
+		camera.z += moveSpeed * Math.cos((camera.mouseX + 90) * Math.PI / 180)
+		camera.x += moveSpeed * Math.sin((camera.mouseX - 90) * Math.PI / 180)
+	}
+	if (userKeys.has("KeyS")) {
+		camera.z += moveSpeed * Math.cos(camera.mouseX * Math.PI / 180)
+		camera.x += moveSpeed * Math.sin((camera.mouseX + 180) * Math.PI / 180)
+	}
+	if (userKeys.has("KeyD")) {
+		camera.z += moveSpeed * Math.cos((camera.mouseX - 90) * Math.PI / 180);
+		camera.x += moveSpeed * Math.sin((camera.mouseX + 90) * Math.PI / 180);
+	}
+	if (userKeys.has("Space")) camera.y += moveSpeed;
+	if (userKeys.has("ShiftLeft")) camera.y -= moveSpeed;
+
+	if (camera.positionMoved) camera.updatePosition();
+
+	window.requestAnimationFrame(time => movePlayer(time, curentTime));
+}
+
+document.body.onclick = () => !document.pointerLockElement && document.body.requestPointerLock({ unadjustedMovement: true });
+document.addEventListener("pointerlockerror", () => console.error("Error locking pointer"));
+
+document.addEventListener("pointerlockchange", () => {
+	if (document.pointerLockElement) document.addEventListener("mousemove", mouseMovement);
+	else document.removeEventListener("mousemove", mouseMovement);
+});
+
+function mouseMovement(event) {
+	camera.mouseX += (event?.movementX ?? 0) * camera.mouseSensitivity;
+	camera.mouseY -= (event?.movementY ?? 0) * camera.mouseSensitivity;
+	camera.mouseY = Math.min(90, Math.max(camera.mouseY, -90));
+
+	camera.updateRotation();
+}
