6defc3a6d17b1d6d0d308f57913cbd2b31aa713a
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-06-14 13:35:25 +0300

    webGPU done

--- JavaScript/WebGPU-API/test-v2/js/code.js
+++ JavaScript/WebGPU-API/test-v2/js/code.js
@@ -1,5 +1,5 @@
 const GRID_SIZE = 32;
-const UPDATE_INTERVAL = 200; // Update every 200ms (5 times/sec)
+const UPDATE_INTERVAL = 20; // Update every 200ms (5 times/sec)
 const WORKGROUP_SIZE = 8;
 let step = 0; // Track how many simulation steps have been run
 
@@ -209,15 +209,38 @@ const simulationShaderModule = device.createShaderModule({
 		@group(0) @binding(2) var<storage, read_write> cellStateOut: array<u32>;
 
 		fn cellIndex(cell: vec2u) -> u32 {
-			return cell.y * u32(grid.x) + cell.x;
+			return (cell.y % u32(grid.y)) * u32(grid.x) +
+						 (cell.x % u32(grid.x));
+		}
+
+		fn cellActive(x: u32, y: u32) -> u32 {
+			return cellStateIn[cellIndex(vec2(x, y))];
 		}
 
 		@compute @workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE})
 		fn computeMain(@builtin(global_invocation_id) cell: vec3u) {
-			if (cellStateIn[cellIndex(cell.xy)] == 1) {
-				cellStateOut[cellIndex(cell.xy)] = 0;
-			} else {
-				cellStateOut[cellIndex(cell.xy)] = 1;
+			let activeNeighbors = cellActive(cell.x+1, cell.y+1) +
+				cellActive(cell.x+1, cell.y) +
+				cellActive(cell.x+1, cell.y-1) +
+				cellActive(cell.x, cell.y-1) +
+				cellActive(cell.x-1, cell.y-1) +
+				cellActive(cell.x-1, cell.y) +
+				cellActive(cell.x-1, cell.y+1) +
+				cellActive(cell.x, cell.y+1);
+
+			let i = cellIndex(cell.xy);
+
+			// Conway's game of life rules:
+			switch activeNeighbors {
+				case 2: { // Active cells with 2 neighbors stay active.
+					cellStateOut[i] = cellStateIn[i];
+				}
+				case 3: { // Cells with 3 neighbors become or stay active.
+					cellStateOut[i] = 1;
+				}
+				default: { // Cells with < 2 or > 3 neighbors become inactive.
+					cellStateOut[i] = 0;
+				}
 			}
 		}
 		`
