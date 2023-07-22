631c016e4c4f679e815ce6d5a8f2e46a93b4fc52
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-07-23 00:47:26 +0300

    naming + color

--- JavaScript/random-rhombus-tilings/code.js
+++ JavaScript/random-rhombus-tilings/code.js
@@ -9,59 +9,59 @@ container.style.transform = `scale(${maxWidth / (size * 100 * 2)})`
 
 for (let y = 0; y < size; y++) {
 	for (let x = 0; x < size; x++) {
-		const blue = document.createElement("div");
-		blue.classList.add("blue");
-		blue.style.left = `${x * tilePixelSize}px`;
-		blue.style.top = `${y * tilePixelSize + x * -tileOffset + topOffset}px`;
-		blue.setAttribute("data-x", size - x - 1);
-		blue.setAttribute("data-y", size - y - 1);
-		blue.setAttribute("data-z", 0);
+		const left = document.createElement("div");
+		left.classList.add("left");
+		left.style.left = `${x * tilePixelSize}px`;
+		left.style.top = `${y * tilePixelSize + x * -tileOffset + topOffset}px`;
+		left.setAttribute("data-x", size - x - 1);
+		left.setAttribute("data-y", size - y - 1);
+		left.setAttribute("data-z", 0);
 
-		const orange = document.createElement("div");
-		orange.classList.add("orange");
-		orange.style.left = `${size * tilePixelSize + x * tilePixelSize}px`;
-		orange.style.top = `${y * tilePixelSize + (x - size + 1) * tileOffset + topOffset}px`;
-		orange.setAttribute("data-x", 0);
-		orange.setAttribute("data-y", size - y - 1);
-		orange.setAttribute("data-z", x);
+		const right = document.createElement("div");
+		right.classList.add("right");
+		right.style.left = `${size * tilePixelSize + x * tilePixelSize}px`;
+		right.style.top = `${y * tilePixelSize + (x - size + 1) * tileOffset + topOffset}px`;
+		right.setAttribute("data-x", 0);
+		right.setAttribute("data-y", size - y - 1);
+		right.setAttribute("data-z", x);
 
-		const green = document.createElement("div");
-		green.classList.add("green");
-		green.style.left = `${x * tilePixelSize + y * tilePixelSize + 60}px`;
-		green.style.top = `${y * tileOffset + (x) * -tileOffset + size * tilePixelSize - 14 + topOffset}px`;
-		green.setAttribute("data-x", size - x - 1);
-		green.setAttribute("data-y", 0);
-		green.setAttribute("data-z", y);
+		const bottom = document.createElement("div");
+		bottom.classList.add("bottom");
+		bottom.style.left = `${x * tilePixelSize + y * tilePixelSize + 60}px`;
+		bottom.style.top = `${y * tileOffset + (x) * -tileOffset + size * tilePixelSize - 14 + topOffset}px`;
+		bottom.setAttribute("data-x", size - x - 1);
+		bottom.setAttribute("data-y", 0);
+		bottom.setAttribute("data-z", y);
 
-		container.append(blue, orange, green);
+		container.append(left, right, bottom);
 	}
 }
 
 const corners = [[0, 0, 0]];
 
 function checkCords(x, y, z) {
-	const blue = container.querySelector(`.blue[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
-	const orange = container.querySelector(`.orange[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
-	const green = container.querySelector(`.green[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
+	const left = container.querySelector(`.left[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
+	const right = container.querySelector(`.right[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
+	const bottom = container.querySelector(`.bottom[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
 
-	return (blue && orange && green);
+	return (left && right && bottom);
 }
 
 function moveUp(x, y, z) {
-	const blue = container.querySelector(`.blue[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
-	const orange = container.querySelector(`.orange[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
-	const green = container.querySelector(`.green[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
+	const left = container.querySelector(`.left[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
+	const right = container.querySelector(`.right[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
+	const bottom = container.querySelector(`.bottom[data-x="${x}"][data-y="${y}"][data-z="${z}"]`);
 
-	green.style.top = `${parseFloat(green.style.top) - tilePixelSize}px`;
-	green.setAttribute("data-y", y + 1);
+	bottom.style.top = `${parseFloat(bottom.style.top) - tilePixelSize}px`;
+	bottom.setAttribute("data-y", y + 1);
 
-	blue.style.top = `${parseFloat(blue.style.top) + tileOffset}px`;
-	blue.style.left = `${parseFloat(blue.style.left) + tilePixelSize}px`;
-	blue.setAttribute("data-z", z + 1);
+	left.style.top = `${parseFloat(left.style.top) + tileOffset}px`;
+	left.style.left = `${parseFloat(left.style.left) + tilePixelSize}px`;
+	left.setAttribute("data-z", z + 1);
 
-	orange.style.top = `${parseFloat(orange.style.top) + tileOffset}px`;
-	orange.style.left = `${parseFloat(orange.style.left) - tilePixelSize}px`;
-	orange.setAttribute("data-x", x + 1);
+	right.style.top = `${parseFloat(right.style.top) + tileOffset}px`;
+	right.style.left = `${parseFloat(right.style.left) - tilePixelSize}px`;
+	right.setAttribute("data-x", x + 1);
 
 	const returnCorners = [];
 
