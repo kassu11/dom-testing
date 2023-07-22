111272c124c19809bbb37d03a7f7f69225d402de
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-07-22 22:52:09 +0300

    less magic number

--- JavaScript/random-rhombus-tilings/code.js
+++ JavaScript/random-rhombus-tilings/code.js
@@ -1,5 +1,7 @@
 const container = document.querySelector("#container");
-const size = 20;
+const size = 10;
+const tilePixelSize = 100;
+const tileOffset = Math.tan(30 * Math.PI / 180) * tilePixelSize;
 
 const maxWidth = 700;
 container.style.transform = `scale(${maxWidth / (size * 100 * 2)})`
@@ -8,22 +10,22 @@ for (let y = 0; y < size; y++) {
 	for (let x = 0; x < size; x++) {
 		const blue = document.createElement("div");
 		blue.classList.add("blue");
-		blue.style.left = `${x * 100}px`;
-		blue.style.top = `${y * 100 + x * -58}px`;
+		blue.style.left = `${x * tilePixelSize}px`;
+		blue.style.top = `${y * tilePixelSize + x * -tileOffset}px`;
 		blue.setAttribute("data-x", x);
 		blue.setAttribute("data-y", y);
 
 		const orange = document.createElement("div");
 		orange.classList.add("orange");
-		orange.style.left = `${size * 100 + x * 100}px`;
-		orange.style.top = `${y * 100 + (x - size + 1) * 58}px`;
+		orange.style.left = `${size * tilePixelSize + x * tilePixelSize}px`;
+		orange.style.top = `${y * tilePixelSize + (x - size + 1) * tileOffset}px`;
 		orange.setAttribute("data-x", x);
 		orange.setAttribute("data-y", y);
 
 		const green = document.createElement("div");
 		green.classList.add("green");
-		green.style.left = `${x * 100 + y * 100 + 60}px`;
-		green.style.top = `${y * 57 + (x) * -58 + size * 100 - 14}px`;
+		green.style.left = `${x * tilePixelSize + y * tilePixelSize + 60}px`;
+		green.style.top = `${y * tileOffset + (x) * -tileOffset + size * tilePixelSize - 14}px`;
 		green.setAttribute("data-x", x);
 		green.setAttribute("data-y", y);
 
