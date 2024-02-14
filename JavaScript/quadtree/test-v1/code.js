a936effc5870befc468f0e7fdd25f5375dd5a9a1
kassu11 <61124141+kassu11@users.noreply.github.com>
2024-02-14 18:46:00 +0200

    Add QuadTree demo and stats

--- JavaScript/quadtree/test-v1/code.js
+++ JavaScript/quadtree/test-v1/code.js
@@ -1,37 +1,59 @@
+const WIDTH = 600;
+const HEIGHT = 600;
+
+const pointCount = document.getElementById("pointCount");
+const totalPointCount = document.getElementById("totalPointCount");
+
+/**
+ * @param {Number} min - The minimum value
+ * @param {Number} max - The maximum value
+ * @returns {Number} - A random number between min and max
+ */
+function random(min, max) {
+	return Math.round(Math.random() * (max - min)) + min;
+}
+
 const canvas = /**  @type {HTMLCanvasElement} */ (document.getElementById("canvas"));
-canvas.width = 2 ** 9;
-canvas.height = 2 ** 9;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 const ctx = canvas.getContext("2d");
 
-const boundary = new Rectangle(0, 0, 2 ** 9, 2 ** 9);
-const tree = new QuadTree(boundary, 4);
+const boundary = new Rectangle(0, 0, WIDTH, HEIGHT);
+const tree = new QuadTree(boundary, 16);
+const searchRange = new Rectangle(random(0, WIDTH), random(0, HEIGHT), random(0, WIDTH), random(0, HEIGHT));
 
-for (let i = 0; i < 20; i++) {
-	const p = new Point(Math.random() * 2 ** 9, Math.random() * 2 ** 9);
+for (let i = 0; i < 1000; i++) {
+	const randomX = Math.abs(WIDTH / 2 + (Math.random() * 2 - 1) ** 5 * WIDTH);
+	const randomY = Math.abs(HEIGHT / 2 + (Math.random() * 2 - 1) ** 5 * HEIGHT);
+	const p = new Point(randomX, randomY);
 	tree.insert(p);
 }
 
 canvas.addEventListener("mousemove", (event) => {
-	if (event.buttons !== 1) return;
-	const p = new Point(event.offsetX, event.offsetY);
-	tree.insert(p);
+	searchRange.x = event.offsetX - searchRange.w / 2;
+	searchRange.y = event.offsetY - searchRange.h / 2;
 
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	tree.show(ctx);
+	if (event.buttons == 1) {
+		const p = new Point(event.offsetX, event.offsetY);
+		tree.insert(p);
+	}
+	update();
 });
-
-const point1 = new Point(100, 50);
-const point2 = new Point(200, 100);
-const point3 = new Point(50, 50);
-const point4 = new Point(50, 50);
-const point5 = new Point(100, 100);
-
-tree.insert(point1);
-tree.insert(point2);
-tree.insert(point3);
-tree.insert(point4);
-tree.insert(point5);
 
 console.log(tree);
 
-tree.show(ctx);
+update();
+
+function update() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	tree.show(ctx);
+	searchRange.draw(ctx, "lime");
+	const queryPoints = tree.query(searchRange);
+
+	pointCount.textContent = String(queryPoints.length);
+	totalPointCount.textContent = String(tree.length);
+
+	queryPoints.forEach((point) => {
+		point.draw(ctx, "lime");
+	});
+}
--- JavaScript/quadtree/test-v1/quadtree.js
+++ JavaScript/quadtree/test-v1/quadtree.js
@@ -2,10 +2,24 @@ class Point {
 	/**
 	 * @param {number} x - The x coordinate of the point
 	 * @param {number} y - The y coordinate of the point
+	 * @property {number} x - The x coordinate of the point
+	 * @property {number} y - The y coordinate of the point
 	 */
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	/**
+	 * @param {CanvasRenderingContext2D} ctx - The 2D rendering context for the canvas
+	 * @param {string} [color] - The color of the point
+	 * @returns {void}
+	 */
+	draw(ctx, color = "#ffffff90") {
+		ctx.beginPath();
+		ctx.fillStyle = color;
+		ctx.arc(this.x, this.y, 1.5, 0, 2 * Math.PI);
+		ctx.fill();
 	}
 }
 
@@ -23,15 +37,47 @@ class Rectangle {
 		this.h = h;
 	}
 
+	/**
+	 * @param {Point} point - The point to check for containment
+	 * @returns {boolean} - Whether the point is contained in the rectangle
+	 */
 	contains(point) {
 		return point.x >= this.x && point.x <= this.x + this.w && point.y >= this.y && point.y <= this.y + this.h;
+	}
+
+	/**
+	 * @param {Rectangle} range - The range to check for intersection
+	 * @returns {boolean} - Whether the rectangle intersects the range
+	 */
+	intersects(range) {
+		return !(range.x > this.x + this.w || range.x + range.w < this.x || range.y > this.y + this.h || range.y + range.h < this.y);
+	}
+
+	/**
+	 * @param {CanvasRenderingContext2D} ctx - The 2D rendering context for the canvas
+	 * @param {string} [color] - The color of the rectangle
+	 * @returns {void}
+	 */
+	draw(ctx, color = "#3d3d3d") {
+		ctx.strokeStyle = color;
+		ctx.strokeRect(this.x, this.y, this.w, this.h);
 	}
 }
 
 class QuadTree {
+	#_length = 0;
+
 	/**
 	 * @param {Rectangle} boundary - The boundary of the quadtree
 	 * @param {number} capacity - The maximum number of points that can be stored in the quadtree
+	 * @property {Rectangle} boundary - The boundary of the quadtree
+	 * @property {number} capacity - The maximum number of points that can be stored in the quadtree
+	 * @property {Point[]} points - The points contained in the quadtree
+	 * @property {boolean} divided - Whether the quadtree is divided into quadrants
+	 * @property {QuadTree} northwest - The northwest quadrant of the quadtree
+	 * @property {QuadTree} northeast - The northeast quadrant of the quadtree
+	 * @property {QuadTree} southwest - The southwest quadrant of the quadtree
+	 * @property {QuadTree} southeast - The southeast quadrant of the quadtree
 	 */
 	constructor(boundary, capacity) {
 		this.boundary = boundary;
@@ -40,11 +86,18 @@ class QuadTree {
 		this.divided = false;
 	}
 
+	get length() {
+		return this.#_length;
+	}
+
 	/**
 	 * @param {Point} point - The point to insert into the quadtree
+	 * @returns {boolean} - Whether the point was successfully inserted into the quadtree
 	 */
 	insert(point) {
 		if (!this.boundary.contains(point)) return false;
+
+		this.#_length++;
 		if (this.points.length < this.capacity) this.points.push(point);
 		else {
 			if (!this.divided) this.subdivide();
@@ -52,6 +105,10 @@ class QuadTree {
 		}
 	}
 
+	/**
+	 * Subdivides the quadtree into four quadrants
+	 * @returns {void}
+	 */
 	subdivide() {
 		const { x, y, w, h } = this.boundary;
 		const halfWidth = w / 2;
@@ -71,19 +128,39 @@ class QuadTree {
 
 	/**
 	 * @param {CanvasRenderingContext2D} ctx - The 2D rendering context for the canvas
+	 * @returns {void}
 	 */
 	show(ctx) {
-		this.boundary && ctx.strokeRect(this.boundary.x, this.boundary.y, this.boundary.w, this.boundary.h);
+		this.boundary?.draw(ctx);
 		if (this.divided) {
 			this.northwest.show(ctx);
 			this.northeast.show(ctx);
 			this.southwest.show(ctx);
 			this.southeast.show(ctx);
 		}
-		// this.points.forEach((point) => {
-		// 	ctx.beginPath();
-		// 	ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
-		// 	ctx.fill();
-		// });
+
+		this.points.forEach((point) => point.draw(ctx));
+	}
+
+	/**
+	 * @param {Rectangle} range - The range to query for points
+	 * @param {?Point[]} [found] - The points found in the range
+	 * @returns {Point[]} - The points found in the range
+	 */
+	query(range, found = []) {
+		if (this.boundary.intersects(range)) {
+			this.points.forEach((point) => {
+				if (range.contains(point)) found.push(point);
+			});
+
+			if (this.divided) {
+				this.northwest.query(range, found);
+				this.northeast.query(range, found);
+				this.southwest.query(range, found);
+				this.southeast.query(range, found);
+			}
+		}
+
+		return found;
 	}
 }
