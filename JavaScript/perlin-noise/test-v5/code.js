15b892ff4a79245c05dd0f4c8f0c998f7dca4603
kassu11 <61124141+kassu11@users.noreply.github.com>
2024-03-08 17:01:35 +0200

    Add Consistent noise

--- JavaScript/perlin-noise/test-v5/code.js
+++ JavaScript/perlin-noise/test-v5/code.js
@@ -19,7 +19,8 @@ class RandomFromCoordinates {
 
 class PerlinNoise {
 	#maxNoise = 255;
-	#padding = 200;
+	#padding = 250;
+	#maxGridSize = 75;
 
 	constructor(seed, width, height) {
 		this.seed = Number(seed) || Math.random() * 10 ** 6;
@@ -33,7 +34,7 @@ class PerlinNoise {
 		const max = 5;
 
 		for (let i = max; i > 1; i--) {
-			const pixelSize = ~~(75 / i);
+			const pixelSize = ~~(this.#maxGridSize / i);
 			const blurSize = ~~((pixelSize / 10) * 4);
 			const layarNoise = this.#generateLayer(pixelSize, blurSize, genX, genY);
 			for (let i = 0, j = 0; i < layarNoise.length; i += 4, j++) {
@@ -55,22 +56,29 @@ class PerlinNoise {
 		blurCanvas.width = canvas.width;
 		blurCanvas.height = canvas.height;
 
+		const calcMove = cords => {
+			const value = ~~(cords / this.#maxGridSize) * this.#maxGridSize;
+			const remainder = value % pixelSize;
+			return [value - remainder, remainder]
+		}
+
+		const [moveX, offsetX] = calcMove(genX);
+		const [moveY, offsetY] = calcMove(genY);
+
 		for (let y = 0; y < canvas.height; y += pixelSize) {
 			for (let x = 0; x < canvas.width; x += pixelSize) {
-				const color = Math.floor(this.coordinatesRandom.from(x - genX, y - genY) * this.#maxNoise);
+				const color = Math.floor(this.coordinatesRandom.from(x + moveX, y + moveY) * this.#maxNoise);
 				ctx.fillStyle = `rgb(${color}, 0, 0)`;
-				ctx.fillRect(x, y, pixelSize, pixelSize);
+				ctx.fillRect(x - offsetX, y - offsetY, pixelSize, pixelSize);
 			}
 		}
 
 		blurContext.filter = `blur(${blurSize}px)`;
 		blurContext.drawImage(canvas, 0, 0);
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		const width = canvas.width - pixelSize * 2;
-		const height = canvas.height - pixelSize * 2;
-		ctx.drawImage(blurCanvas, pixelSize, pixelSize, width, height, 0, 0, canvas.width, canvas.height);
+		ctx.drawImage(blurCanvas, 0, 0, canvas.width, canvas.height);
 
-		return ctx.getImageData(this.#padding / 2, this.#padding / 2, this.width, this.height).data;
+		return ctx.getImageData(this.#padding / 2 + genX % this.#maxGridSize, this.#padding / 2 + genY % this.#maxGridSize, this.width, this.height).data;
 	}
 }
 
@@ -87,11 +95,23 @@ function badHash(string) {
 }
 
 let seed = 234234;
+let inputX = 0;
+let inputY = 0;
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d", { alpha: false });
 
-document.querySelector("input").addEventListener("input", (e) => {
+document.querySelector("#cordsX").addEventListener("input", (e) => {
+	inputX = Number(e.target.value);
+	renderCanvas();
+});
+
+document.querySelector("#cordsY").addEventListener("input", (e) => {
+	inputY = Number(e.target.value);
+	renderCanvas();
+});
+
+document.querySelector("#seed").addEventListener("input", (e) => {
 	seed = badHash(e.target.value);
 	renderCanvas();
 });
@@ -99,10 +119,12 @@ document.querySelector("input").addEventListener("input", (e) => {
 renderCanvas();
 
 function renderCanvas() {
-	const perlinNoise = new PerlinNoise(seed, 100, 100);
-	const noise = perlinNoise.generate(50, 50);
+	const perlinNoise = new PerlinNoise(seed, 600, 600);
+	const noise = perlinNoise.generate(inputX, inputY);
+	canvas.width = perlinNoise.width;
+	canvas.height = perlinNoise.height;
 
-	const imageData = new ImageData(100, 100);
+	const imageData = new ImageData(perlinNoise.width, perlinNoise.height);
 	for (let i = 0, j = 0; i < noise.length; i++, j += 4) {
 		imageData.data[j] = noise[i];
 		imageData.data[j + 1] = noise[i];
