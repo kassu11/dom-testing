efffb6afc780a76bcc57201fb52701686104f289
kassu11 <61124141+kassu11@users.noreply.github.com>
2024-03-28 12:05:44 +0200

    Fix nesting points

--- JavaScript/vector-math/2d-irregular-polygon-area/code.js
+++ JavaScript/vector-math/2d-irregular-polygon-area/code.js
@@ -1,27 +1,45 @@
+const visualize = document.querySelector("#visualize");
 const output = document.querySelector("output")
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+
 canvas.width = 600;
 canvas.height = 600;
 
-const polygon = [[10, 10], [450, 50], [500, 500], [200, 50], [10, 550]]
+const polygon = [[125,32.125],[489,40.125],[546,156.125],[521,415.125],[418,559.125],[186,572.125],[25,455.125],[48,166.125],[145,80.125],[328,71.125],[129,234.125],[175,452.125],[406,458.125],[242,303.125],[537,234.125],[259,201.125],[443,156.125],[444,107.125],[348,58.125],[190,53.125],[42,58.125],[53,18.125]]
 
-function render(close = true) {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.strokeStyle = "white"
-	if(polygon.length == 0) return;
-
-	const [first, ...rest] = polygon;
+function render(ctx, points, close = true, color = "white", clear = true, drawPoints = false) {
+	if(clear) ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.strokeStyle = color
+	
+	if(points.length == 0) return;
+	const [first, ...rest] = points;
 	ctx.beginPath()
 	ctx.moveTo(first[0], first[1]);
 
 	rest.forEach(point => {
 		ctx.lineTo(point[0], point[1])
 	});
+
+	
 	
 	if(close) ctx.lineTo(first[0], first[1]);
 	ctx.stroke();
+
+	if (drawPoints) {
+		points.forEach(([x, y], i) => {
+			ctx.moveTo(x, y);
+			ctx.beginPath();
+			ctx.lineWidth = 3;
+			if (i === 1) ctx.strokeStyle = "lime";
+			else ctx.strokeStyle = color;
+			ctx.arc(x, y, 5, 0, 2 * Math.PI)
+			ctx.stroke();
+		})
+	}
+
+	
 }
 
 let editing = false;
@@ -35,13 +53,13 @@ document.addEventListener("click", e => {
 	const {top, left} = canvas.getBoundingClientRect();
 	polygon.push([e.x - left, e.y - top]);
 	calculateArea();
-	render(false);
+	render(ctx, polygon, false);
 });
 
 document.addEventListener("mousemove", e => {
 	if(polygon.length === 0 || !editing) return;
 	const {top, left} = canvas.getBoundingClientRect();
-	render(false)
+	render(ctx, polygon, false)
 
 	ctx.lineTo(e.x - left, e.y - top);
 	ctx.stroke();
@@ -50,13 +68,14 @@ document.addEventListener("mousemove", e => {
 document.addEventListener("keydown", e => {
 	if(e.code == "Enter") {
 		editing = false;
-		render();
+		render(ctx, polygon);
 	}
 })
 
 
 function calculateArea() {
 	output.textContent = 0;
+	visualize.textContent = "";
 	let polygonCopy = structuredClone(polygon);
 	if(polygonCopy.length < 3) return 0;
 
@@ -70,8 +89,9 @@ function calculateArea() {
 		const line = [triangle[0], triangle[2]].flat();
 		if(!isTriangleValid(polygonCopy, line, triangle)) continue;
 
+		visualizeCaluculation(polygonCopy, triangle)
+
 		area += calculateTriangleArea(triangle)
-		console.log("????")
 		polygonCopy = polygonCopy.filter(p => p !== triangle[1]);
 		triangle.length = 0;
 		i = 0;
@@ -120,8 +140,14 @@ function isTriangleValid(polygon, line, triangle) {
 
 	const x = (triangle[0][0] + triangle[1][0] + triangle[2][0]) / 3;
 	const y = (triangle[0][1] + triangle[1][1] + triangle[2][1]) / 3;
+	if(!isPointInsidePolygon(polygon, x, y)) return false;
 
-	return isPointInsidePolygon(polygon, x, y);
+	const isValid = polygon.every(point => {
+		if(triangle.some(t => point === t)) return true;
+		return !isPointInsidePolygon(triangle, point[0], point[1])
+	})
+
+	return isValid;
 }
 
 function findIntersection([x1, y1, x2, y2], [x3, y3, x4, y4]) {
@@ -154,4 +180,23 @@ function vectorInRange([x1, y1, x2, y2], x, y) {
 	return true;
 }
 
-render();
+render(ctx, polygon);
+
+function visualizeCaluculation(polygons, triangle, area) {
+	const newCanvas = document.createElement("canvas");
+	newCanvas.width = canvas.width;
+	newCanvas.height = canvas.height;
+	const newCtx = newCanvas.getContext("2d");
+	render(newCtx, polygons);
+	render(newCtx, triangle, true, "red", false, true);
+
+	const x = (triangle[0][0] + triangle[1][0] + triangle[2][0]) / 3;
+	const y = (triangle[0][1] + triangle[1][1] + triangle[2][1]) / 3;
+
+	newCtx.strokeStyle = "white";
+	newCtx.beginPath();
+	newCtx.arc(x, y, 5, 0, 2 * Math.PI);
+	newCtx.stroke();
+
+	visualize.appendChild(newCanvas);
+}
